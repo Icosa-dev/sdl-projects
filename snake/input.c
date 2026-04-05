68e2ac204f130bcbc310ce037004d6dcd195aaec
Icosa-dev <ljcottendev@gmail.com>
2026-04-04 23:02:05 -0400

    Removed redundant code

--- snake/input.c
+++ snake/input.c
@@ -42,63 +42,6 @@ void get_user_input(struct snake *snake, bool *running)
 	}
 }
 
-/* Check if a snake segment is at a certain coordinate */
-static bool segment_at(struct snake *snake, float x, float y)
-{
-	for (int i = 1; i < snake->size; i++)
-	{
-		if (get_segment(snake, i)->x == x && get_segment(snake, i)->y == y)
-			return true;
-	}
-
-	return false;
-}
-
-/* Check if a move is valid and will not cause collision */
-static bool is_valid(struct snake *snake, float upper_bound_x,
-	float upper_bound_y, float lower_bound_x, float lower_bound_y,
-	enum direction move_direction)
-{
-	float current_x = get_segment(snake, 0)->x;
-	float current_y = get_segment(snake, 0)->y;
-	float next_x = current_x;
-	float next_y = current_y;
-
-	switch (move_direction)
-	{
-	case UP:
-		next_y -= CELL_SIZE;
-		break;
-	case DOWN:
-		next_y += CELL_SIZE;
-		break;
-	case LEFT:
-		next_x -= CELL_SIZE;
-		break;
-	case RIGHT:
-		next_x += CELL_SIZE;
-		break;
-	}
-
-	/* Check if the new coordinates hit the body */
-	for (int i = 1; i < snake->size; i++)
-	{
-		if (next_x == get_segment(snake, i)->x &&
-			next_y == get_segment(snake, i)->y)
-			return false;
-	}
-
-	/* Check if the new coordinates hit the board boundaries */
-	if (next_x <= lower_bound_x || next_y <= lower_bound_y ||
-		next_x >= upper_bound_x || next_y >= upper_bound_y)
-		return false;
-
-	if (segment_at(snake, next_x, next_y))
-		return false;
-
-	return true;
-}
-
 static enum direction get_longest_direction(float x, float y,
 	float upper_bound_x, float upper_bound_y)
 {
@@ -138,33 +81,6 @@ static enum direction get_longest_direction(float x, float y,
 	{
 		return LEFT;
 	}
-}
-
-/*
- * This function will set snake to a different direction than
- * provided if the given one is not valid
- */
-static void move_snake(struct snake *snake, enum direction direction,
-	float upper_bound_x, float upper_bound_y, float lower_bound_x,
-	float lower_bound_y)
-{
-#define IS_VALID(direction)                                      \
-	is_valid(snake, upper_bound_x, upper_bound_y, lower_bound_x, \
-		lower_bound_y, direction)
-
-	if (IS_VALID(direction))
-		snake->direction = direction;
-
-	else if (IS_VALID(UP))
-		snake->direction = UP;
-	else if (IS_VALID(DOWN))
-		snake->direction = DOWN;
-	else if (IS_VALID(LEFT))
-		snake->direction = LEFT;
-	else if (IS_VALID(RIGHT))
-		snake->direction = RIGHT;
-
-#undef IS_VALID
 }
 
 /*
