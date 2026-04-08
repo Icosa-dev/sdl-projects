25262c7f907aecc5201077e68cc3b73994259762
Icosa-dev <ljcottendev@gmail.com>
2026-04-07 20:15:48 -0400

    Refactored sv

---
+++ sv/main.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "sort.h"
+
+#define PROGRAM_NAME "snake"
+#define DESCRIPTION "A basic snake game implementation in SDL3"
+#define AUTHORS "LJC"
+#define VERSION "v0.1.0"
+#define MIT_LICENSE_TEXT                                                                \
+	"Copyright (c) " __DATE__ " " AUTHORS "\n\n"                                        \
+	"Permission is hereby granted, free of charge, to any person obtaining a copy\n"    \
+	"of this software and associated documentation files (the \"Software\"), to deal\n" \
+	"in the Software without restriction, including without limitation the rights\n"    \
+	"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"       \
+	"copies of the Software, and to permit persons to whom the Software is\n"           \
+	"furnished to do so, subject to the following conditions:\n\n"                      \
+	"The above copyright notice and this permission notice shall be included in all\n"  \
+	"copies or substantial portions of the Software.\n\n"                               \
+	"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"    \
+	"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"        \
+	"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"     \
+	"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"          \
+	"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"   \
+	"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"   \
+	"SOFTWARE."
+
+#define STREQ(str1, str2) (strcmp(str1, str2) == 0)
+
+static void
+print_version(void)
+{
+	printf("%s: %s (%s)\n%s\n", PROGRAM_NAME, DESCRIPTION, VERSION,
+		MIT_LICENSE_TEXT);
+	exit(0);
+}
+
+static void
+print_help(void)
+{
+	printf("Usage: sv [OPTIONS]\n");
+	printf("Options:\n");
+	printf("\t-h, --help\t\t\tPrint this help message and exit\n");
+	printf("\t-v, --version\t\t\tPrint the version message and exit\n");
+	printf("\t--delay\t\t\t\tSet the delay of the program in milliseconds\n");
+	printf("\t--bubble\t\t\tSort using the bubble sort algorithm\n");
+	printf("\t--selection\t\t\tSort using the selection sort algorithm\n");
+	printf("\t--insertion\t\t\tSort using the insertion sort algorithm\n");
+	printf("\t--quick\t\t\t\tSort using the quicksort algorithm\n");
+	exit(0);
+}
+
+static void
+generate_random_array(int arr[], int array_size)
+{
+	srand(time(NULL));
+
+	for (int i = 0; i < array_size; i++)
+		arr[i] = i + 1;
+
+	for (int i = array_size - 1; i > 0; i--)
+	{
+		int j = rand() % (i + 1);
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
+
+static void
+check_event(void)
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+		if (event.type == SDL_EVENT_QUIT)
+		{
+			SDL_Quit();
+			exit(0);
+		}
+}
+
+int
+main(int argc, char **argv)
+{
+	const int column_width = 5;
+	const int array_size = 201;
+	const int window_width = array_size * column_width;
+	const int window_height = window_width;
+
+	int delay = 25;
+
+	enum algorithm algorithm = BUBBLE;
+
+	if (argc > 1)
+	{
+		for (int i = 0; i < argc; i++)
+		{
+			const char *arg = argv[i];
+
+			if (STREQ(arg, "-h") || STREQ(arg, "--help"))
+				print_help();
+			else if (STREQ(arg, "-v") || STREQ(arg, "--version"))
+				print_version();
+			else if (STREQ(arg, "--bubble"))
+				algorithm = BUBBLE;
+			else if (STREQ(arg, "--selection"))
+				algorithm = SELECTION;
+			else if (STREQ(arg, "--insertion"))
+				algorithm = INSERTION;
+			else if (STREQ(arg, "--quick"))
+				algorithm = QUICK;
+
+			if (i + 1 < argc)
+			{
+				const char *value = argv[i + 1];
+				if (STREQ(arg, "--delay"))
+					delay = atoi(value);
+			}
+		}
+	}
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow(PROGRAM_NAME, window_width,
+		window_height, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, 0);
+
+	int array[array_size];
+	generate_random_array(array, array_size);
+
+	bool running = true;
+	bool sorted = false;
+	while (running)
+	{
+		check_event();
+
+		if (!sorted)
+		{
+			switch (algorithm)
+			{
+			case BUBBLE:
+				bubble_sort(renderer, array, array_size, column_width,
+					window_width, window_height, delay);
+				break;
+			case SELECTION:
+				selection_sort(renderer, array, array_size, column_width,
+					window_width, window_height, delay);
+				break;
+			case INSERTION:
+				insertion_sort(renderer, array, array_size, column_width,
+					window_width, window_height, delay);
+				break;
+			case QUICK:
+				quicksort(renderer, array, array_size, 0, array_size,
+					column_width, window_width, window_height, delay);
+				break;
+			}
+			sorted = true;
+		}
+
+		display_array(renderer, array, array_size, column_width, window_height,
+			window_height, delay, sorted);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
---
+++ sv/sort.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef SORT_H_
+#define SORT_H_
+
+#include <stdbool.h>
+typedef struct SDL_Renderer SDL_Renderer;
+
+/**
+ * @brief Display an array as a bar graph
+ * 
+ * @param renderer The renderer to draw the graph too
+ * @param array The underlying data as an array
+ * @param array_size The size of the array
+ * @param column_width The width of a bar column
+ * @param window_width The width of the window to display to
+ * @param window_height The height of the window to display to
+ * @param delay How many milliseconds to delay after displaying
+ * @param sorted If true renderer bars as green, else white
+ */
+extern void display_array(SDL_Renderer *renderer, int array[], int array_size,
+	int column_width, int window_width, int window_height, int delay,
+	bool sorted);
+
+/**
+ * @brief Sort and display an array using the bubble sort algorithm
+ * 
+ * @param renderer The renderer to draw the graph too
+ * @param array The underlying data as an array
+ * @param array_size The size of the array
+ * @param column_width The width of a bar column
+ * @param window_width The width of the window to display to
+ * @param window_height The height of the window to display to
+ * @param delay How many milliseconds to delay after displaying
+ */
+extern void bubble_sort(SDL_Renderer *renderer, int array[], int array_size,
+	int column_width, int window_width, int window_height, int delay);
+
+/**
+ * @brief Sort and display an array using the selection sort algorithm
+ * 
+ * @param renderer The renderer to draw the graph too
+ * @param array The underlying data as an array
+ * @param array_size The size of the array
+ * @param column_width The width of a bar column
+ * @param window_width The width of the window to display to
+ * @param window_height The height of the window to display to
+ * @param delay How many milliseconds to delay after displaying
+ */
+extern void selection_sort(SDL_Renderer *renderer, int array[], int array_size,
+	int column_width, int window_width, int window_height, int delay);
+
+/**
+ * @brief Sort and display an array using the insertion sort algorithm
+ * 
+ * @param renderer The renderer to draw the graph too
+ * @param array The underlying data as an array
+ * @param array_size The size of the array
+ * @param column_width The width of a bar column
+ * @param window_width The width of the window to display to
+ * @param window_height The height of the window to display to
+ * @param delay How many milliseconds to delay after displaying
+ */
+extern void insertion_sort(SDL_Renderer *renderer, int array[], int array_size,
+	int column_width, int window_width, int window_height, int delay);
+
+/**
+ * @brief Sort and display an array using the quicksort algorithm
+ * 
+ * @param renderer The renderer to draw the graph too
+ * @param array The underlying data as an array
+ * @param array_size The size of the array
+ * @param column_width The width of a bar column
+ * @param window_width The width of the window to display to
+ * @param window_height The height of the window to display to
+ * @param delay How many milliseconds to delay after displaying
+ */
+extern void quicksort(SDL_Renderer *renderer, int array[], int array_size,
+	int low, int high, int column_width, int window_width, int window_height,
+	int delay);
+
+enum algorithm
+{
+	BUBBLE,
+	SELECTION,
+	INSERTION,
+	QUICK
+};
+
+#endif
