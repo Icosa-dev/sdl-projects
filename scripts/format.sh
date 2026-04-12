f38020afaf57821c65ee003a50a991f3ec6b8944
Icosa-dev <ljcottendev@gmail.com>
2026-04-12 01:05:50 -0400

    (scripts) Moved format.sh to scripts directory

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,6 +3,8 @@
 # Auto format all project header and source files with
 # clang-format 
 
+cd ..
+
 for dir in */; do
     echo "Formatting: $dir"
     clang-format -i "$dir"*.c "$dir"*.h 2>/dev/null
